25f01fcc98c0d60f5423e340c287d777df60eac4
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2024-12-06 13:27:00 +0100

    first commit

---
+++ src/packages/pkg1/R/hello.R
@@ -0,0 +1,10 @@
+#' Print a Hello World message
+#'
+#' This function prints "Hello, world!" to the console.
+#'
+#' @return None. This function is called for its side effect.
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
---
+++ src/packages/pkg2/R/goodbye.R
@@ -0,0 +1,10 @@
+#' Print a Goodbye World message
+#'
+#' This function prints "Goodbye, world!" to the console.
+#'
+#' @return None. This function is called for its side effects.
+#' @export
+#' @export
+goodbye <- function() {
+  print("Goodbye, world!")
+}
